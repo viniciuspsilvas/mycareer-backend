af1fc8543a4441fe8bea8420c0f1c7efc70664fd
Vinicius <viniciuspsilvas@gmail.com>
2023-03-26 15:58:39 +1000

    Removing /refresh_token rest api

--- apps/graphql/src/server.ts
+++ apps/graphql/src/server.ts
@@ -42,39 +42,6 @@ export const createApolloServer = async (options = { port: 3000 }) => {
 
   app.get('/hc', (req: Request, res: Response) => res.json({ status: 'ok' }))
 
-  app.post('/refresh_token', async (req, res) => {
-    const token = req.body.refreshToken
-    if (!token) {
-      return res.send({ ok: false, accessToken: '' })
-    }
-
-    let payload: any = null
-    try {
-      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!)
-    } catch (err) {
-      console.log(err)
-      return res.send({ ok: false, accessToken: '' })
-    }
-
-    // token is valid and
-    // we can send back an access token
-    const user = await prisma.user.findUnique({ where: { id: payload.userId } })
-
-    if (!user) {
-      return res.send({ ok: false, accessToken: '' })
-    }
-
-    if (user.tokenVersion !== payload.tokenVersion) {
-      return res.send({ ok: false, accessToken: '' })
-    }
-
-    // Not using cookie for refreshToken anymore,
-    // instead the new token is being returned.
-    // sendRefreshToken(res, createRefreshToken(user))
-
-    return res.send({ ok: true, accessToken: createAccessToken(user), refreshToken: createRefreshToken(user) })
-  })
-
   const schema = await buildSchema({
     resolvers: [HealthCheckResolver, ...resolvers],
     emitSchemaFile: path.resolve(__dirname, './graphql/generated/schema.graphql'),
